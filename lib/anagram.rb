163e62cf68ad4b84446281ca4cd16826ffb04ac9
ddbrennan <ddbren@gmail.com>
2017-11-24 15:32:31 -0500

    add anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select {|w| w.split("").sort == word.split("").sort}
+  end
+
+end
