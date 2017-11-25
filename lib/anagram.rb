9bfcc3ef92e96dc30e408c31d422c2f415e36695
Marvin Clerge <clergemarvin@gmail.com>
2017-11-25 15:29:33 -0500

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagrams.select do |a_word|
+      a_word.chars.sort == self.word.chars.sort
+    end
+  end
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,4 +1,5 @@
-describe 'Anagram' do 
+require 'pry'
+describe 'Anagram' do
   it 'should detect no matches' do
     diaper = Anagram.new('diaper')
     expect(diaper.match(%w(hello world zombies pants dipper))).to eq([])
