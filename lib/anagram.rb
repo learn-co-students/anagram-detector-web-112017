687fff11d17f78bf8c7296e82399b75ba34f3010
Flatiron School <leslie.hsu@flatironschool.com>
2017-11-27 06:54:09 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    matches = []
+    array_of_words.each do |word|
+      tester = word.split("").sort
+      if tester == @word.split("").sort
+        matches << word
+      end
+    end
+    matches
+  end
+
+
+
+end
