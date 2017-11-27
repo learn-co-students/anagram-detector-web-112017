824e878c036f153a4e46917c0f6e744e43296f1b
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-11-26 20:47:33 -0500

    complete anagram detector class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+   array.select do |test|
+     test.chars.sort.join == word.chars.sort.join
+   end
+end
+
+end
